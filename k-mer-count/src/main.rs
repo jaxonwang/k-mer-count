5afb7e9f1edaf0ac9a191e42c67fde44edfb4caf
harazono <6312081@ed.tus.ac.jp>
2022-05-08 13:47:11 +0900

    WIP

--- k-mer-count/src/main.rs
+++ k-mer-count/src/main.rs
@@ -1,76 +1,60 @@
 extern crate bio;
+extern crate rdxsort;
 
 use std::fs::File;
-use bio::io::fasta;
-use bio::io::fasta::{Record, FastaRead};
+use bio::io::fasta::{Record, FastaRead, Reader};
 use std::str::from_utf8;
 
 fn main() {
     let file = File::open("sample.fasta").expect("Error during opening the file");
-    let mut reader = Box::new(fasta::Reader::new(file));
+    let mut reader = Reader::new(file);
     let mut record = Record::new();
 
     let l_len = 27;
     let r_len = 27;
-    let mut lr_chunk = Vec::new();
-    for dna_chunk_size in 80..141 {
-        let m_len = dna_chunk_size - l_len - r_len;
-        let mut window_start = 0;
-        loop{
-            reader.read(&mut record).unwrap();
-            if record.is_empty(){
-                break;
+    let mut lr_chunk:Vec<String> = Vec::new();
+    let mut window_start: usize;
+    let mut l_start: usize;
+    let mut l_end:   usize;
+    let mut r_start: usize;
+    let mut r_end:   usize;
+    let mut m_len:   usize;
+
+    loop {
+        reader.read(&mut record).unwrap();
+        if record.is_empty(){
+            break;
+        }
+        for dna_chunk_size in 80..141 {
+            window_start = 0;
+            loop{
+                m_len = dna_chunk_size - l_len - r_len;
+                l_start = window_start;
+                l_end   = l_start + l_len;
+                r_start = l_end + m_len;
+                r_end   = r_start + r_len;
+                window_start += 1;
+
+                if r_end > record.seq().len(){
+                    break;
+                }
+                /*
+                print!("record id: {}\twindow start: {}\tDNA chunk size: {}", record.id(), window_start, dna_chunk_size);
+                print!("\tL start: {}\t L end: {}\tR start: {}\tR end: {}", l_start, l_end, r_start, r_end);
+                println!("\trecord length: {}\t {}", record.seq().len(), r_end > record.seq().len());
+                */
+                let l = &record.seq()[l_start..l_end];
+                let r = &record.seq()[r_start..r_end];
+                //print!("{:?}", from_utf8(l).unwrap());
+                //println!("{:?}", from_utf8(r).unwrap());
+                let tmp_lr_chunk = from_utf8(l).unwrap().clone().to_owned() + from_utf8(r).unwrap();
+                lr_chunk.push(tmp_lr_chunk);
             }
-            let l_start: usize = window_start;
-            let l_end:   usize = l_start + l_len;
-            let r_start: usize = l_end   + m_len;
-            let r_end:   usize = r_start + r_len;
-            window_start += 1;
-            if r_end > record.seq().len().try_into().unwrap(){
-                break;
-            }
-            let l = &record.seq()[l_start..l_end];
-            let r = &record.seq()[r_start..r_end];
-/*
-            let spacer1: String = " ".repeat(l_start);
-            let spacer2: String = " ".repeat(m_len);
-            println!("  {:?}\n{:?}{:?}{:?}{:?}",
-                from_utf8(&record.seq()).unwrap(),
-                spacer1,
-                from_utf8(l).unwrap(),
-                spacer2,
-                from_utf8(r).unwrap()
-                );
-*/
-            let tmp_lr_chunk = from_utf8(l).unwrap().clone().to_owned() + from_utf8(r).unwrap();
-            //lr_chunk.push(format!("{:?}{:?}", l, r));
-            lr_chunk.push(tmp_lr_chunk);
+            //println!("{} loop {} DONE", record.id(), dna_chunk_size);
         }
     }
     lr_chunk.sort();
     for each_chunk in lr_chunk.iter() {
-        //println!("{:?}", from_utf8(each_chunk).unwrap());
         println!("{}", each_chunk);
     }
-    //println!("{:?}", lr_chunk);
 }
-
-/*
-    for DNA_chunk_size in range(80, 140 + 1):
-        M_len = DNA_chunk_size - L_len - R_len
-        for each_read in reads:
-            read_length = len(each_read)
-            i = 0
-            while True:
-                L_start = i
-                L_end   = L_start + L_len
-                R_start = L_end + M_len
-                R_end   = R_start + R_len
-                L = each_read[L_start:L_end]
-                R = each_read[R_start:R_end]
-                if len(R) != R_len:
-                    break
-                #print(f"L_start: {L_start}\tL_end: {L_end}\tlen(L): {len(L)}\tR_start: {R_start}\tR_end: {R_end}\tlen(R): {len(R)}")
-                reads_LR.append(L + R)
-                i = i + 1
-*/
