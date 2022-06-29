4ae461d1adad67e44f47bd22b3795a8bc1970202
harazono <6312081@ed.tus.ac.jp>
2022-06-29 22:39:53 +0900

    WIP

---
+++ kmer_count/src/counting_bloomfilter_util.rs
@@ -0,0 +1,254 @@
+use crate::encoder_util::decode_u128_2_dna_seq;
+use crate::encoder_util::encode_dna_seq_2_u128;
+use crate::encoder_util::decode_u128_2_occurence;
+use crate::encoder_util::L_LEN;
+use crate::encoder_util::R_LEN;
+
+use rand::Rng;
+use std::fs::File;
+use sha2::Sha256;
+use sha2::Digest;
+
+//use bio::io::fastq::Reader as fqReader;
+//use bio::io::fastq::Record as fqRecord;
+use bio::io::fasta::Reader as faReader;
+use bio::io::fasta::Record as faRecord;
+//use bio::io::fastq::FastqRead;
+use bio::io::fasta::FastaRead;
+
+pub const BLOOMFILTER_TABLE_SIZE: usize = u32::MAX as usize + 1;
+pub const THRESHOLD_OCCURENCE: u64 = 100;
+
+
+pub fn counting_bloom_filter(path: &str) -> Box<[u64; BLOOMFILTER_TABLE_SIZE]>{
+    let mut window_start: usize;
+    let mut l_start: usize;
+    let mut l_end:   usize;
+    let mut r_start: usize;
+    let mut r_end:   usize;
+    let mut m_len:   usize;
+    let mut loop_cnt:usize = 0;
+    println!("{}", loop_cnt);
+    let mut ret_array: Box<[u64; BLOOMFILTER_TABLE_SIZE]> = Box::new([0; BLOOMFILTER_TABLE_SIZE]);
+    println!("{}", loop_cnt);
+
+    let file = File::open(path).expect("Error during opening the file");
+    println!("{}", loop_cnt);
+
+    let mut reader = faReader::new(file);
+    let mut record = faRecord::new();
+    let mut buf: u64 = 0;
+    let mut lr_string: [u8;L_LEN + R_LEN] = [64; L_LEN + R_LEN];
+    println!("{}", loop_cnt);
+
+    let mut rng = rand::thread_rng();
+
+    loop {
+        println!("{}", loop_cnt);
+        reader.read(&mut record).unwrap();
+        if record.is_empty(){
+            break;
+        }
+        eprintln!("1st loop: {:09?}, current record id:{:?}\tlength: {:?}", loop_cnt, record.id(), record.seq().len());
+        loop_cnt += 1;
+        for dna_chunk_size in 80..141 {
+            window_start = 0;
+            loop{
+                m_len = dna_chunk_size - L_LEN - R_LEN;
+                l_start = window_start;
+                l_end   = l_start + L_LEN;
+                r_start = l_end + m_len;
+                r_end   = r_start + R_LEN;
+
+                if r_end > record.seq().len(){
+                    break;
+                }
+                let l = &record.seq()[l_start..l_end];
+                let r = &record.seq()[r_start..r_end];
+                for i in 0..L_LEN{
+                    lr_string[i] = l[i];
+                }
+                for i in 0..R_LEN{
+                    lr_string[i + L_LEN] = r[i];
+                }
+                let table_indice:[u32;8] = hasher(&lr_string);
+                let tmp: u64 = count_occurence_from_counting_bloomfilter_table(&ret_array, &lr_string);
+
+
+                for i in 0..8{
+                    if ret_array[table_indice[i] as usize] == u64::MAX{
+                        //incrementしない
+                    }else{
+                        if rng.gen::<u64>() < (u64::MAX >> (64 - ret_array[table_indice[i] as usize].leading_zeros())){
+                            ret_array[table_indice[i] as usize] += 1;
+                        }
+                    }
+                }
+
+/*
+                if tmp < u64::MAX{
+                    //incrementしない
+                }else{
+                    if tmp == 0{
+                        //必ずincrementする
+                        for i in 0..8{
+                            ret_array[table_indice[i] as usize] += 1;
+                        }
+                    }else{//0 < tmp < u64::MAX
+                        for i in 0..8{
+                            //ret_array[table_indice[i] as usize]をみて、サイコロと比べる
+                            if rand::random::<u64>() < (u64::MAX >> (64 - ret_array[table_indice[i] as usize].leading_zeros())){
+                                ret_array[table_indice[i] as usize] += 1;
+                            }
+                        }
+                    }
+                }
+*/
+                window_start += 1;
+            }
+        }
+    }
+    return ret_array;
+}
+
+pub fn count_occurence_from_counting_bloomfilter_table(counting_bloomfilter_table: &Box<[u64; BLOOMFILTER_TABLE_SIZE]>, query: &[u8;L_LEN + R_LEN]) -> u64{
+    let indice: [u32;8] = hasher(query);
+    let mut retval: u64 = u64::MAX;
+    for index in indice{
+        if counting_bloomfilter_table[index as usize] < retval{
+            retval = counting_bloomfilter_table[index as usize];
+        }
+    }
+    return retval;
+}
+
+pub fn hasher(source: &[u8;L_LEN + R_LEN]) -> [u32;8]{
+    let mut ret_val: [u32;8] = [0;8];
+    let mut hasher = Sha256::new();
+    hasher.update(source);
+    let result = hasher.finalize();
+    let sha256_bit_array = result.as_slice();//&[u8;32]
+    for i in 0..8{
+        for j in 0..4{
+            ret_val[i] += sha256_bit_array[i * 8 + j] as u32;
+            ret_val[i] <<= 8;
+        }
+    }
+    return ret_val;
+}
+
+//2週目。出現頻度がある閾値を越えるk-merの個数を返す。
+//3週目ではこの個数を受けて、vecのメモリを確保して出力用vecを用意して、再びファイルを舐める。
+pub fn number_of_high_occurence_kmer(source_table: &Box<[u64; BLOOMFILTER_TABLE_SIZE]>, path: &str) -> u64{
+    let mut retval: u64 = 0;
+    let mut window_start: usize;
+    let mut l_start: usize;
+    let mut l_end:   usize;
+    let mut r_start: usize;
+    let mut r_end:   usize;
+    let mut m_len:   usize;
+    let mut loop_cnt:usize = 0;
+
+    let file = File::open(path).expect("Error during opening the file");
+    let mut reader = faReader::new(file);
+    let mut record = faRecord::new();
+    let mut buf: u64 = 0;
+    let mut lr_string: [u8;L_LEN + R_LEN] = [64; L_LEN + R_LEN];
+
+    loop {
+        reader.read(&mut record).unwrap();
+        if record.is_empty(){
+            break;
+        }
+        eprintln!("2nd loop: {:09?}, current record id:{:?}\tlength: {:?}", loop_cnt, record.id(), record.seq().len());
+        loop_cnt += 1;
+        for dna_chunk_size in 80..141 {
+            window_start = 0;
+            loop{
+                m_len = dna_chunk_size - L_LEN - R_LEN;
+                l_start = window_start;
+                l_end   = l_start + L_LEN;
+                r_start = l_end + m_len;
+                r_end   = r_start + R_LEN;
+                window_start += 1;
+
+                if r_end > record.seq().len(){
+                    break;
+                }
+                let l = &record.seq()[l_start..l_end];
+                let r = &record.seq()[r_start..r_end];
+                for i in 0..L_LEN{
+                    lr_string[i] = l[i];
+                }
+                for i in 0..R_LEN{
+                    lr_string[i + L_LEN] = r[i];
+                }
+                let table_indice:[u32;8] = hasher(&lr_string);
+                let tmp: u64 = count_occurence_from_counting_bloomfilter_table(&source_table, &lr_string);
+                if tmp >= THRESHOLD_OCCURENCE{
+                    retval = retval + 1;
+                }
+            }
+        }
+    }
+    return retval;
+}
+
+//3週目。
+pub fn pick_up_high_occurence_kmer(source_table: &Box<[u64; BLOOMFILTER_TABLE_SIZE]>, path: &str, max_size_of_vec: u64) -> Vec<u128>{
+    let mut retval: Vec<u128> = vec![0; max_size_of_vec.try_into().unwrap()];
+    let mut retval_index: usize = 0;
+    let mut window_start: usize;
+    let mut l_start: usize;
+    let mut l_end:   usize;
+    let mut r_start: usize;
+    let mut r_end:   usize;
+    let mut m_len:   usize;
+    let mut loop_cnt:usize = 0;
+
+    let file = File::open(path).expect("Error during opening the file");
+    let mut reader = faReader::new(file);
+    let mut record = faRecord::new();
+    let mut buf: u64 = 0;
+    let mut lr_string: [u8;L_LEN + R_LEN] = [64; L_LEN + R_LEN];
+
+    loop {
+        reader.read(&mut record).unwrap();
+        if record.is_empty(){
+            break;
+        }
+        eprintln!("3rd loop: {:09?}, current record id:{:?}\tlength: {:?}", loop_cnt, record.id(), record.seq().len());
+        loop_cnt += 1;
+        for dna_chunk_size in 80..141 {
+            window_start = 0;
+            loop{
+                m_len = dna_chunk_size - L_LEN - R_LEN;
+                l_start = window_start;
+                l_end   = l_start + L_LEN;
+                r_start = l_end + m_len;
+                r_end   = r_start + R_LEN;
+                window_start += 1;
+
+                if r_end > record.seq().len(){
+                    break;
+                }
+                let l = &record.seq()[l_start..l_end];
+                let r = &record.seq()[r_start..r_end];
+                for i in 0..L_LEN{
+                    lr_string[i] = l[i];
+                }
+                for i in 0..R_LEN{
+                    lr_string[i + L_LEN] = r[i];
+                }
+                //ここら辺に、閾値回数以上出現するk-merを処理するコードを書く
+                let table_indice:[u32;8] = hasher(&lr_string);
+                let tmp: u64 = count_occurence_from_counting_bloomfilter_table(&source_table, &lr_string);
+                if tmp >= THRESHOLD_OCCURENCE{//2^10相当。本当は引数で基準を変えられるようにしたい。
+                    retval[retval_index] = encode_dna_seq_2_u128(&lr_string);
+                    retval_index += 1;
+                }
+            }
+        }
+    }
+    return retval;
+}
