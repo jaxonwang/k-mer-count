c6164b3e91226c8822702e0c56cc5daad93acce0
harazono <6312081@ed.tus.ac.jp>
2022-07-25 13:01:20 +0900

    add argument parser

--- kmer_count/src/main.rs
+++ kmer_count/src/main.rs
@@ -1,42 +1,15 @@
 extern crate bio;
 extern crate rdxsort;
+extern crate getopts;
+use getopts::Options;
 
-use std::env;
-//use std::fs;
-//use std::fs::File;
-//use std::str::from_utf8;
-//use std::io::{BufRead, BufReader};
-//use std::path::Path;
-//use std::collections::hash_map::DefaultHasher;
-//use sha2::{Sha256, Sha512, Digest};
-
-
-//use bio::io::fastq::Reader as fqReader;
-//use bio::io::fastq::Record as fqRecord;
-//use bio::io::fasta::Reader as faReader;
-//use bio::io::fasta::Record as faRecord;
-//use crate::bio::io::fastq::FastqRead;
-//use crate::bio::io::fasta::FastaRead;
-
-//use rdxsort::*;
-//use bit_reverse::ParallelReverse;
-//use anyhow::Result;
-//use flate2::read::MultiGzDecoder;
-
+use std::{env, process};
 use voracious_radix_sort::{RadixSort};
-
-//use rand::prelude::*;
-
-//use kmer_count::encoder_util::decode_u128_2_dna_seq;
-//use kmer_count::encoder_util::encode_dna_seq_2_u128;
-//use kmer_count::encoder_util::decode_u128_2_occurence;
 use kmer_count::counting_bloomfilter_util::L_LEN;
 use kmer_count::counting_bloomfilter_util::R_LEN;
 use kmer_count::counting_bloomfilter_util::BLOOMFILTER_TABLE_SIZE;
 use kmer_count::counting_bloomfilter_util::{build_counting_bloom_filter, number_of_high_occurence_kmer, pick_up_high_occurence_kmer};
 
-
-//const TOW_SQ20: u128 = 2_u128.pow(20);
 
 
 fn decode_u128_2_dna_seq(source:&u128, char_size: usize) -> Vec<u8>{
@@ -87,45 +60,79 @@ fn decode_u128_r(source: &u128) -> [u8; R_LEN]{
     return result;
 }
 
-
+fn print_usage(program: &str, opts: &Options) {
+    let brief = format!("Usage: {} FILE [options]", program);
+    print!("{}", opts.usage(&brief));
+    process::exit(0);
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let  input_path = &args[1];
-    //let output_path = &args[2];
+    let program = args[0].clone();
 
-    eprintln!("input  file: {:?}",  input_path);
-    //eprintln!("output file: {:?}", output_path);
+    let mut opts = Options::new();
+    opts.optopt("o", "output", "set output file name", "NAME");
+    opts.optopt("t", "thread", "number of threads to use for radix sort", "THREAD");
+    opts.optopt("a", "threshold", "threshold for hyper log counter", "THRESHOLD");
+    opts.optflag("h", "help", "print this help menu");
 
-//    let file = File::open(input_path).expect("Error during opening the file");
-//    let mut reader = faReader::new(file);
-//    let mut record = faRecord::new();
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => { m }
+        Err(f) => { panic!("{}", f.to_string()) }
+    };
+    if matches.opt_present("h") {
+        print_usage(&program, &opts);
+        return;
+    }
 
-    eprintln!("loading {:?} done", input_path);
+    let input_file  = if !matches.free.is_empty() {
+        matches.free[0].clone()
+    } else {
+        print_usage(&program, &opts);
+        return;
+    };
+
+    let output_file = matches.opt_str("o");
+
+    let threads = if matches.opt_present("t") {
+        matches.opt_str("t").unwrap().parse::<usize>().unwrap()
+    }else{
+        8
+    };
+
+    let threshold = if matches.opt_present("a") {
+        matches.opt_str("a").unwrap().parse::<u64>().unwrap()
+    }else{
+        8
+    };
+
+
+    eprintln!("input  file: {:?}",  input_file);
+    eprintln!("loading {:?} done", input_file);
 
     //1段目
     eprintln!("start calling build_counting_bloom_filter");
-    let counting_bloom_filter_table: Box<[u64; BLOOMFILTER_TABLE_SIZE]> = build_counting_bloom_filter(input_path);
+    let counting_bloom_filter_table: Box<[u64; BLOOMFILTER_TABLE_SIZE]> = build_counting_bloom_filter(&input_file);
     eprintln!("finish calling build_counting_bloom_filter");
 
     //2段目
     eprintln!("start calling number_of_high_occurence_kmer");
-    let (high_occr_bloomfilter_table, occurence) = number_of_high_occurence_kmer(&counting_bloom_filter_table, input_path, 8);
+    let (high_occr_bloomfilter_table, occurence) = number_of_high_occurence_kmer(&counting_bloom_filter_table, &input_file, threshold);
     eprintln!("finish calling number_of_high_occurence_kmer");
     //3段目
 
     eprintln!("Vec size is {}", occurence);
     eprintln!("start calling pick_up_high_occurence_kmer");
     let occr_with_mergin = ((occurence as f64) * 1.2).ceil() as usize;
-    let mut high_occurence_kmer: Vec<u128> = pick_up_high_occurence_kmer(&high_occr_bloomfilter_table, input_path, occr_with_mergin);
+    let mut high_occurence_kmer: Vec<u128> = pick_up_high_occurence_kmer(&high_occr_bloomfilter_table, &input_file, occr_with_mergin);
     eprintln!("finish calling pick_up_high_occurence_kmer");
 
+    //sortする
+    high_occurence_kmer.voracious_mt_sort(threads);
 
-    high_occurence_kmer.voracious_mt_sort(8);
-    let mut previous_kmer: u128 = 0;
+/*
     let mut previous_l_kmer: [u8; L_LEN] = [b'A'; L_LEN];
     let mut current_l_kmer:  [u8; L_LEN] = [b'A'; L_LEN];
-
     for each_kmer in high_occurence_kmer{
         current_l_kmer = decode_u128_l(&each_kmer);
         if current_l_kmer != previous_l_kmer{
@@ -133,21 +140,22 @@ fn main() {
         }
         previous_l_kmer = current_l_kmer;
     }
-
-
+*/
+    let mut previous_kmer: u128 = 0;
     let mut cnt = 0;
-/*
     for each_kmer in high_occurence_kmer{
         if previous_kmer != each_kmer{
             println!("{:?}", String::from_utf8(decode_u128_2_dna_seq(&each_kmer, 54)).unwrap());
             cnt += 1;
+/*
             if cnt >= 1000{
                 break;
             }
+*/
         }
         previous_kmer = each_kmer;
     }
-*/
+
 
 
 }
