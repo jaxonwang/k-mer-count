628bb97cef199dd9e40bdae20add83a9b538949f
harazono <6312081@ed.tus.ac.jp>
2022-08-10 00:24:18 +0900

    WIP: add new flag

--- kmer_count/src/main.rs
+++ kmer_count/src/main.rs
@@ -2,15 +2,15 @@ extern crate bio;
 extern crate rdxsort;
 extern crate getopts;
 use getopts::Options;
-use std::fs::File;
-use std::io::Write;
 use std::{env, process};
+use std::fs;
+use std::fs::File;
+use std::io::{BufWriter, Write};
 use voracious_radix_sort::{RadixSort};
 use kmer_count::counting_bloomfilter_util::L_LEN;
 use kmer_count::counting_bloomfilter_util::R_LEN;
 use kmer_count::counting_bloomfilter_util::BLOOMFILTER_TABLE_SIZE;
 use kmer_count::counting_bloomfilter_util::{build_counting_bloom_filter, number_of_high_occurence_kmer, pick_up_high_occurence_kmer};
-
 
 
 fn decode_u128_2_dna_seq(source:&u128, char_size: usize) -> Vec<u8>{
@@ -75,6 +75,7 @@ fn main() {
     opts.optopt("o", "output", "set output file name", "NAME");
     opts.optopt("t", "thread", "number of threads to use for radix sort. default value is 8.", "THREAD");
     opts.optopt("a", "threshold", "threshold for hyper log counter. default value is 8.", "THRESHOLD");
+    opts.optflag("b", "binary", "outputs binary file");
     opts.optflag("r", "only-num", "outputs only total number of k-mer");
     opts.optflag("h", "help", "print this help menu");
 
@@ -151,9 +152,12 @@ fn main() {
 */
     eprintln!("start writing to output file: {:?}", &output_file);
 
-    let mut w = File::create(&output_file).unwrap();
+    //let mut w = File::create(&output_file).unwrap();
+    let mut w = BufWriter::new(fs::File::create(&output_file).unwrap());
     let mut previous_kmer: u128 = 0;
     let mut cnt = 0;
+    let mut buf_array: [u8; 16] = [0; 16];
+    let mut buf_num: u128;
 
     if matches.opt_present("r") {
         for each_kmer in high_occurence_kmer{
@@ -162,11 +166,21 @@ fn main() {
             }
             previous_kmer = each_kmer;
         }
-        writeln!(&mut w, "k-mer count: {}\tthreshold: {}\tinput file {:?}", cnt, threshold, &output_file).unwrap();
+        writeln!(&mut w, "k-mer count: {}\tthreshold: {}\tinput file {:?}", cnt, threshold, &input_file).unwrap();
     }else{
         for each_kmer in high_occurence_kmer{
             if previous_kmer != each_kmer{
-                writeln!(&mut w, "{:?}", String::from_utf8(decode_u128_2_dna_seq(&each_kmer, 54)).unwrap()).unwrap();
+                if matches.opt_present("r") {
+                    buf_num = each_kmer;
+                    for i in 0..16{
+                        buf_array[15 - i] = u8::try_from(buf_num & 0xFF).unwrap();
+                        buf_num >>= 8;
+                    }
+                    w.write(&buf_array).unwrap();
+                }else{
+                    writeln!(&mut w, "{:?}", String::from_utf8(decode_u128_2_dna_seq(&each_kmer, 54)).unwrap()).unwrap();
+                }
+                cnt += 1;
             }
             previous_kmer = each_kmer;
         }
