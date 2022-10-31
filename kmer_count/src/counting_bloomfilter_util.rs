28980695ad3a8fd2fdece4bdcc8c3adff5f43f97
harazono <6312081@ed.tus.ac.jp>
2022-11-01 02:04:35 +0900

    WIP

--- kmer_count/src/counting_bloomfilter_util.rs
+++ kmer_count/src/counting_bloomfilter_util.rs
@@ -344,9 +344,9 @@ pub fn pick_up_high_occurence_kmer(source_table: &Box<[bool; BLOOMFILTER_TABLE_S
             let mut l_offset_1: usize;
             let mut l_offset_2: usize;
             let mut l_offset_3: usize;
-            eprintln!("\nL_LEN: {}, start: {}, end: {}", L_LEN, l_window_start, l_window_end);
+            eprintln!("\nL_LEN: {}, start: {}, end: {}, l_window_cnt: {}", L_LEN, l_window_start, l_window_end, l_window_cnt);
             let (l_has_poly_base, l_offset_1)     = current_sequence.has_poly_base(l_window_start, l_window_end);
-            eprintln!("\nL_LEN: {}, start: {}, end: {}", L_LEN, l_window_start, l_window_end);
+            eprintln!("L_LEN: {}, start: {}, end: {}, l_window_cnt: {}", L_LEN, l_window_start, l_window_end, l_window_cnt);
 
             let (l_has_simple_repeat, l_offset_2) = current_sequence.has_simple_repeat(l_window_start, l_window_end);
             let (l_has_2base_repeat, l_offset_3)  = current_sequence.has_2base_repeat(l_window_start, l_window_end);
